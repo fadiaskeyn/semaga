b801da7ac3a3fff450c8318ce38d33a83ebb90d8
RicoMuliaPutra <rijalfak9@gmail.com>
2024-04-22 23:24:03 +0700

    nambah route mapel

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -6,7 +6,7 @@
                     <h1>DASHBOARD ADMIN</h1>
                     <aside class=" flex gap-2 mt-1 flex-col md:flex-row ">
                         <a class="btn" href="{{route('user.index')}}">manajemen guru</a>
-                        <a class="btn" href="#">manajemen mata pelajaran</a>
+                        <a class="btn" href="{{route('mapels.index')}}">manajemen mata pelajaran</a>
                         <a class="btn" href="#">manajemen kelas</a>
                         <a class="btn" href="{{route('students.index')}}">manajemen siswa</a>
                         <a class="btn" href="{{route('transgressions.index')}}">mengelola pelanggaran</a>
